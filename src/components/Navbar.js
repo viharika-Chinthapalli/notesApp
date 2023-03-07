ca304f87bed46234c93ceafa55ad06bdc9802269
Viharika <viharika807@gmail.com>
2023-03-07 18:30:31 +0530

    step-1

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1 font-weight-bold">Navbar</span>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
